d1fc28c756e1ca893ca5c41c6a277cf1e3945abd
jmiguel-cit <jmiguel@ciandt.com>
2018-08-05 15:09:19 -0300

    versao inicial com os bancos configurados

---
+++ ABInBev Devices/ABInBev Devices/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ABInBev Devices
+//
+//  Created by Joao Miguel Godoy Faccio on 05/08/2018.
+//  Copyright © 2018 ciandt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ABInBev Devices/Pods/Target Support Files/Pods-ABInBev Devices/Pods-ABInBev Devices-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ABInBev_Devices : NSObject
+@end
+@implementation PodsDummy_Pods_ABInBev_Devices
+@end
